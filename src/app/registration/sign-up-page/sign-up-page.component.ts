ede3ab9ec7f930c051e4b3f47f69d65575ead21e
SeKryGe <algerd008@gmail.com>
2023-05-10 23:30:06 +0300

    add auth/regist func

--- src/app/registration/sign-up-page/sign-up-page.component.ts
+++ src/app/registration/sign-up-page/sign-up-page.component.ts
@@ -11,14 +11,13 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class SignUpPageComponent {
 
-
   user!: User
   signupForm!: FormGroup
 
   constructor(
     public auth: AuthService,
-    public fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    public fb: FormBuilder
     ) { }
 
   ngOnInit() {
@@ -53,7 +52,16 @@ export class SignUpPageComponent {
   get lastName() {return this.signupForm.get('lastName')}
 
   signUp() {
-    throw new Error('Method not implemented.');
+    this.auth.signUp({
+      "email": this.email?.value,
+      "password": this.password?.value,
+      "firstName": this.firstName?.value,
+      "lastName": this.lastName?.value
+    }).subscribe(res => {
+      res
+      this.router.navigate(['/'])})
+    
+    this.signupForm.reset()
   }
 
   setNames(user:any) {
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,46 @@ import { Observable } from 'rxjs';
 export class AuthService {
   user?: Observable<User>
 
-  constructor() { }
+  host = 'http://localhost:3000/auth';
+
+  constructor(private http: HttpClient) { }
 
   
   loginIn(email: string, password: string) {
-    throw new Error('Method not implemented.');
+    return this.http.post<any>(`${this.host}/login`, {
+      "email": email,
+      "password": password
+    })
+    .pipe(
+      map((token) => {
+        console.log(token)
+        localStorage.setItem("token", token.token)
+        return token
+      })
+      )
   }
-  signUp(email: string, password: string, firstName:string, lastName:string) {
-    throw new Error('Method not implemented.');
+
+  loggedIn() {
+    return !!localStorage.getItem("token")
+  }
+
+  signUp(user: User) {
+    return this.http.post<any>(`${this.host}/signup`, {
+      "email":user.email,
+      "password":user.password,
+      "firstName":user.firstName,
+      "lastName":user.lastName
+    }).pipe(
+      map( token => {
+        console.log(token)
+        localStorage.setItem("token", token.token)
+        return token
+      })
+      )
+  }
+
+  logOut() {
+    localStorage.removeItem('token')
   }
 
   updateUser(user: string, data: { firstName: string; lastName: string; }) {
